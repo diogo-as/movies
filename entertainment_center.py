b9feb8533a4cdcb4f103491b37bc3a5a02562375
Diogo Alves de Sousa <diogoas.bsi@gmail.com>
2019-01-11 14:27:06 -0200

    List of films inserted

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,24 +1,38 @@
 import fresh_tomatoes
 import media
 
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that comes to life",
-                        "https://bit.ly/2M2uCP8",
-                        "https://bit.ly/2ctDg5y")
+mi_fallout = media.Movie("Mission: Impossible - Fallout",
+                        "Ethan Hunt race against time after a mission gone wrong",
+                        "https://upload.wikimedia.org/wikipedia/pt/2/21/Mission_Impossible_%E2%80%93_Fallout.jpg",
+                        "https://www.youtube.com/watch?v=XiHiW4N7-bo")
 
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "https://pt.wikipedia.org/wiki/Avatar_(filme)#/media/File:Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+the_angel = media.Movie("The Angel",
+                     "The egyptian spy who saved Israel",
+                     "https://m.media-amazon.com/images/M/MV5BMTEzOTY5ODA2MDReQTJeQWpwZ15BbWU4MDI5NDkyMTYz._V1_SY1000_CR0,0,682,1000_AL_.jpg",
+                     "https://www.youtube.com/watch?v=z0q2WbXQWbw")
+
+avengers_infinity_war = media.Movie("Avengers: Infinity War",
+                      "The avengers and their allies against Thanos",
+                      "https://upload.wikimedia.org/wikipedia/pt/9/90/Avengers_Infinity_War.jpg",
+                      "https://www.youtube.com/watch?v=hA6hldpSTF8")
 
 spectre = media.Movie("007 Spectre",
-                      "007 against Spectre",
+                      "007 against Spectre, a terrorist organization",
                       "https://upload.wikimedia.org/wikipedia/pt/1/1a/Spectre-007.png",
                       "https://www.youtube.com/watch?v=z4UDNzXD3qA")
 
+resistence_banker = media.Movie("The Resistence Banker",
+                      "A banker in occupied Amsterdam setting up a clandestine bank to finance resistance",
+                      "https://upload.wikimedia.org/wikipedia/en/d/dd/The_Resistance_Banker.jpg",
+                      "https://www.youtube.com/watch?v=31pzuZSvzvU")
+
+the_revenant = media.Movie("The Revenant",
+                      "A man fights for survival after being mauled by a bear and left to dead",
+                      "https://upload.wikimedia.org/wikipedia/pt/0/03/TheRevenant-Poster.png",
+                      "https://www.youtube.com/watch?v=LoebZZ8K5N0")
 
 #print (spectre.storyline)
 
 #spectre.show_trailer()
-movies = [toy_story,avatar,spectre]
+movies = [mi_fallout, the_angel, avengers_infinity_war, spectre, resistence_banker, the_revenant]
 fresh_tomatoes.open_movies_page(movies)
